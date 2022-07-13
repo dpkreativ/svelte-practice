e68632e7be30b8843ada554c07513261f83eff30
Divine Orji <dpkreativ@gmail.com>
2022-07-13 13:31:57 +0100

    Add open graph meta tags for testing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
 <svelte:head>
 	<title>Hello Divine</title>
+	<meta property="og:title" content="Svelte Learn" />
+	<meta property="og:site_name" content="Svelte Learn" />
+	<meta property="og:url" content="link-to-hosted-site.com" />
+	<meta property="og:description" content="Learning how to use Svelte" />
+	<meta
+		property="og:image"
+		content="https://res.cloudinary.com/dpkreativ/image/upload/v1657564635/sample_ctunnp.jpg"
+	/>
 </svelte:head>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
